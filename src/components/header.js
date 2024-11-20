e14f9d30589de06a103e8e6f4d146929ed663d00
schizist <damnco17@gmail.com>
2024-11-19 22:05:42 -0700

    Refactor Header component, adjust styles for consistent height, and increase monogram font size

--- src/components/header.js
+++ src/components/header.js
@@ -2,20 +2,18 @@ import * as React from "react"
 import { Link } from "gatsby"
 import * as styles from "../components/index.module.css"
 
+
+const headheight = "6em"
 const Header = ({ siteTitle }) => (
   <header
-    style={{
-      position: "relative",
-      maxHeight: "100vh",
-      backgroundColor: "yellow",
-
-      backgroundSize: "cover",
-      backgroundPosition: "center top",
-      backgroundRepeat: "no-repeat",
-      display: "flex",
-      flexDirection: "column",
-      // maxWidth: "2900px"
-    }}
+  style={{
+    position: "relative",
+    height: headheight, // Ensure the header has consistent height
+    backgroundColor: "yellow",
+    display: "flex",
+    alignItems: "center",  // Vertically centers the children
+    padding: "0 1em",  // Add some padding for spacing
+  }}
   >
     <Link
       to="/"
@@ -27,12 +25,19 @@ const Header = ({ siteTitle }) => (
       <img 
         src={require("../../src/images/dumbLogo.png").default}
         alt={siteTitle} 
-        style={{ height: `50px`,
+        style={{ 
+          height: '100%',
           display: 'flex',
-         }} />
+          alignItems: 'center',
+         }} />  
       <span 
         className={styles.monogram} 
-        style={{ display: 'flex' }}>
+        style={{ 
+          display: 'flex',
+          alignItems: 'center',
+          height: '100%',
+          marginLeft: '0',
+         }}>
         DC<b style={{color: 'var(--color-primary)'}}>C</b>
       </span>
     </Link>
