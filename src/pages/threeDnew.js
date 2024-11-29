0ca297e278466e2ba5101812b64732b8332fe1c2
schizist <damnco17@gmail.com>
2024-11-29 14:37:48 -0700

    Add react-helmet for improved SEO management, update background color in main styles, and rename threeDnew page component for clarity

--- src/pages/threeDnew.js
+++ src/pages/threeDnew.js
@@ -3,12 +3,17 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 import App from "../components/App"
 
-const New = () => (
+const IndexPage = () => (
     <Layout>
   <React.StrictMode>
     <App />
   </React.StrictMode>
   </Layout>
 )
-export const Head = () => <Seo title="New" />
-export default New
+export const Head = () => (
+    <Seo
+      title="3D Viewer Carousel Page"
+      description="Explore our 3D models in a carousel setup, featuring unique projects."
+    />
+  );
+export default IndexPage
